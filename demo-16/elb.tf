7b94bad83728918283c563222d4bb7a6966f1a7e
whizmate <whizmate@thongs-mbp.fios-router.home>
2017-10-15 20:23:40 -0400

    updated

--- demo-16/elb.tf
+++ demo-16/elb.tf
@@ -2,7 +2,7 @@ resource "aws_elb" "my-elb" {
   name = "my-elb"
   subnets = ["${aws_subnet.main-public-1.id}", "${aws_subnet.main-public-2.id}"]
   security_groups = ["${aws_security_group.elb-securitygroup.id}"]
- listener {
+  listener {
     instance_port = 80
     instance_protocol = "http"
     lb_port = 80
