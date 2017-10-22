62c633da10ee865204081fbbf1614acacae9333a
whizmate <whizmate@thongs-mbp.fios-router.home>
2017-10-22 08:16:58 -0400

    updated

--- docker-demo-2/myapp.tf
+++ docker-demo-2/myapp.tf
@@ -47,7 +47,7 @@ resource "aws_ecs_service" "myapp-service" {
   name = "simple-web"
   cluster = "${aws_ecs_cluster.example-cluster.id}"
   task_definition = "${aws_ecs_task_definition.myapp-task-definition.arn}"
-  desired_count = 1
+  desired_count = 2
   iam_role = "${aws_iam_role.ecs-service-role.arn}"
   depends_on = ["aws_iam_policy_attachment.ecs-service-attach1"]
 
